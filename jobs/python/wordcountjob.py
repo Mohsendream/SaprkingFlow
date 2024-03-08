d2ba4bd3b0009cf1afe9a478254a0196a50a0362
Mohsendream <madiounimohsen@gmail.com>
2024-03-08 08:36:13 +0100

    add  pythonwordcount

---
+++ jobs/python/wordcountjob.py
@@ -0,0 +1,11 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("PythonWordCountJob").getOrCreate()
+
+text="Mohsen Mohsen Mohsen Mohsen Hello World data data Engineer Data"
+words=spark.sparkContext.parallelize(text.split(" "))
+counts=words.map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
+for wc in counts.collect():
+    print(wc[0], wc[1])
+
+spark.stop()
